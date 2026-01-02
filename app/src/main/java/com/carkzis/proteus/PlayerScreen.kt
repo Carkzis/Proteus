85c3b3882c0f3104c66581745360b92ef6ad0002
Carkzis <marc.jowett@gmail.com>
2026-01-02 10:44:50 +0000

    Put UI logic in screen from route

--- app/src/main/java/com/carkzis/proteus/PlayerScreen.kt
+++ app/src/main/java/com/carkzis/proteus/PlayerScreen.kt
@@ -7,6 +7,7 @@ import android.os.Build
 import androidx.activity.ComponentActivity
 import androidx.annotation.OptIn
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
@@ -45,92 +46,107 @@ fun PlayerRoute(
 ) {
     val exoPlayer = playerViewModel.playerState.collectAsStateWithLifecycle()
     val context = LocalContext.current
+    val inPipMode = rememberIsInPipMode()
 
-    Row {
-        Button(
-            onClick = {
-                playerViewModel.createPlayerWithMediaItems(
-                    context,
-                    "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
-                )
-            },
-            modifier = modifier
-        ) {
-            Text(text = "Play Video")
+    PlayerScreen(
+        modifier = modifier,
+        exoPlayer = exoPlayer.value,
+        isInPipMode = inPipMode,
+        onPlayerLaunch = {
+            playerViewModel.createPlayerWithMediaItems(
+                context,
+                "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+            )
         }
-    }
-
-    Box(modifier.fillMaxSize()) {
-        exoPlayer.value?.let {
-            PlayerScreen(exoPlayer = it)
-        }
-    }
+    )
 }
 
 @OptIn(UnstableApi::class)
 @Composable
 fun PlayerScreen(
     modifier: Modifier = Modifier,
-    exoPlayer: ExoPlayer,
+    exoPlayer: ExoPlayer?,
+    isInPipMode: Boolean,
+    onPlayerLaunch: () -> Unit
 ) {
-    var duration by remember { mutableLongStateOf(exoPlayer.duration.takeIf { it >= 0 } ?: 0L) }
-    var position by remember { mutableLongStateOf(0L) }
-    val inPipMode = rememberIsInPipMode()
-
-    LaunchedEffect(Unit) {
-        while (true) {
-            position = exoPlayer.currentPosition
-            delay(500L)
-        }
-    }
-
-    LaunchedEffect(
-        exoPlayer.isPlaying
+    Column(
+        modifier = modifier.fillMaxSize()
     ) {
-        if (exoPlayer.isPlaying) {
-            duration = exoPlayer.duration
-        }
-    }
-
-    Box(
-        modifier = modifier.padding(8.dp)
-    ) {
-        PlayerSurface(
-            player = exoPlayer,
-            surfaceType = SURFACE_TYPE_SURFACE_VIEW,
-            modifier = Modifier
-                .fillMaxWidth()
-                .aspectRatio(16f / 9f)
-                .align(Alignment.Center)
-        )
-
-        val context = LocalContext.current
-        Button(
-            modifier = Modifier.align(Alignment.TopEnd),
-            onClick = {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                context.findActivity().enterPictureInPictureMode(
-                    PictureInPictureParams.Builder().build()
-                )
-            } else {
-                Log.i("PROTEUS_TAG", "API does not support PiP")
+        if (!isInPipMode) {
+            Button(
+                onClick = onPlayerLaunch,
+                modifier = Modifier
+            ) {
+                Text(text = "Play Video")
             }
-        }) {
-            Text(text = "Enter PiP mode!")
         }
 
-        Slider(
-            value = position.toFloat(),
-            onValueChange = {
-                exoPlayer.isScrubbingModeEnabled = true
-                exoPlayer.seekTo(it.toLong())
-                            },
-            valueRange = 0f..duration.toFloat(),
-            onValueChangeFinished = {
-                exoPlayer.isScrubbingModeEnabled = false
-            },
-            modifier = Modifier.fillMaxWidth().padding(8.dp).align(Alignment.BottomCenter)
-        )
+        exoPlayer?.let {
+            var duration by remember {
+                mutableLongStateOf(exoPlayer.duration.takeIf { it >= 0 } ?: 0L)
+            }
+            var position by remember { mutableLongStateOf(0L) }
+
+            LaunchedEffect(Unit) {
+                while (true) {
+                    position = exoPlayer.currentPosition
+                    delay(500L)
+                }
+            }
+
+            LaunchedEffect(
+                exoPlayer.isPlaying
+            ) {
+                if (exoPlayer.isPlaying) {
+                    duration = exoPlayer.duration
+                }
+            }
+
+            Box(
+                modifier = Modifier.padding(8.dp)
+            ) {
+                PlayerSurface(
+                    player = exoPlayer,
+                    surfaceType = SURFACE_TYPE_SURFACE_VIEW,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .aspectRatio(16f / 9f)
+                        .align(Alignment.Center)
+                )
+
+                val context = LocalContext.current
+
+                if (!isInPipMode) {
+                    Button(
+                        modifier = Modifier.align(Alignment.TopEnd),
+                        onClick = {
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                                context.findActivity().enterPictureInPictureMode(
+                                    PictureInPictureParams.Builder().build()
+                                )
+                            } else {
+                                Log.i("PROTEUS_TAG", "API does not support PiP")
+                            }
+                        }) {
+                        Text(text = "Enter PiP mode!")
+                    }
+
+                    Slider(
+                        value = position.toFloat(),
+                        onValueChange = {
+                            exoPlayer.isScrubbingModeEnabled = true
+                            exoPlayer.seekTo(it.toLong())
+                        },
+                        valueRange = 0f..duration.toFloat(),
+                        onValueChangeFinished = {
+                            exoPlayer.isScrubbingModeEnabled = false
+                        },
+                        modifier = Modifier.fillMaxWidth().padding(8.dp)
+                            .align(Alignment.BottomCenter)
+                    )
+                }
+            }
+        }
     }
 }
 
